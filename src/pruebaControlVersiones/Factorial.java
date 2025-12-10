20aeb29c92083cc3db525d550e441c30b2e3c918
rlobmon782 <rlobmon782@g.educaand.es>
2025-12-10 17:55:06 +0100

    Versión con Rebase

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -23,7 +23,8 @@ public class Factorial {
 		} else {
 			System.out.println("Has introducido el número: " + num);
 		}
-		System.out.println("RESULTADO - El factorial del número " + num + " es " + factorialBucle(num));
+
+		System.out.println("RESULTADO - El factorial del número " + num + " es " + factorialRec(num));
 		sc.close();
 	}
 
